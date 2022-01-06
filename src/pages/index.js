4d994815871bf9e9b3931787513732a351cd8653
Hasibullah khan <hasibpak@gmail.com>
2022-01-06 23:43:42 +0400

    add contant for hellow world

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,9 +2,8 @@ import * as React from "react"
 export default function Home() {
   return (
     <div>
-      <h1>
-        Hellow world from gatsby developer Hasibullah Khan
-      </h1>
+      <h1>Hellow world from gatsby developer Hasibullah Khan</h1>
+      <h1>new update comming soon </h1>
     </div>
   )
 }
